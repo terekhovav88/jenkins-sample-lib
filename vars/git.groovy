bf3c6d9221142d45d2547e0a740e9497a2cca439
terekhovav88 <terehov88@icloud.com>
2021-03-29 14:01:11 +0300

    up

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,19 +1,23 @@
 import org.foo.SampleClass
 
 
-def age(int age){
-    def test = new SampleClass()
-    test.increaseAge(age)
-    println(test.name +" "+ test.age+" years old")
+def gitClone(String branch, String cred, String url){
+    git branch: ${branch}, credentialsId: ${cred}, url: ${url}
 }
 
-def info(message) {
-    echo "INFO: ${message}"
-}
-
-def warning(message) {
-    echo "WARNING: ${message}"
-}
+//def age(int age){
+//    def test = new SampleClass()
+//    test.increaseAge(age)
+//    println(test.name +" "+ test.age+" years old")
+//}
+//
+//def info(message) {
+//    echo "INFO: ${message}"
+//}
+//
+//def warning(message) {
+//    echo "WARNING: ${message}"
+//}
 
 //static void main(String[] args) {
 //    age(10)
