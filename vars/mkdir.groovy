d4ecb85e50f087f8c17fca9a81a0701a0e3345c5
terekhovav88 <terehov88@icloud.com>
2021-04-07 20:33:33 +0300

    up

--- vars/mkdir.groovy
+++ vars/mkdir.groovy
@@ -2,5 +2,6 @@ def call(String path_dir){
     sh "mkdir -p ${path_dir}"
     dir(path_dir) {
         sh 'pwd'
+        sh 'git init'
     }
 }
--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -2,6 +2,6 @@ def call(Map stageParams) {
     checkout([
             $class: 'GitSCM',
             branches: [[name:  stageParams.branch ]],
-            userRemoteConfigs: [[ url: stageParams.url ]]
+            userRemoteConfigs: [[credentialsId: stageParams.cred, url: stageParams.url ]]
     ])
 }
